2447d3ee0747e315cf9ad907d223194eab361f4d
R1300-W-4-Stud <R1300-W-4-Stud@ad.itmo.ru>
2023-02-15 02:35:14 +0300

    last commit on 15 feb

--- src/main/java/managers/commandLine/commands/AddElement.java
+++ src/main/java/managers/commandLine/commands/AddElement.java
@@ -2,21 +2,34 @@ package managers.commandLine.commands;
 
 import managers.CollectionManager;
 import managers.commandLine.Console;
+import managers.commandLine.ConsoleColors;
 import managers.commandLine.ExitCode;
+import models.forms.InvalidForm;
+import models.forms.StudyGroupForm;
 
 public class AddElement extends Command{
     private CollectionManager collectionManager;
     private Console console;
 
     public AddElement(Console console, CollectionManager collectionManager) {
-        super("add {element}", "добавить новый элемент в коллекцию");
+        super("add", "{element} добавить новый элемент в коллекцию");
         this.collectionManager = collectionManager;
         this.console = console;
     }
 
     @Override
     public ExitCode execute(String[] args) {
-
-        return ExitCode.OK;
+        while (true) {
+            try {
+                console.println(ConsoleColors.toColor("Создание объекта StudyGroup", ConsoleColors.PURPLE));
+                collectionManager.addElement(new StudyGroupForm(console, collectionManager).build());
+                console.println(ConsoleColors.toColor("Создание объекта StudyGroup окончено успешно!", ConsoleColors.PURPLE));
+                return ExitCode.OK;
+            } catch (InvalidForm invalidForm) {
+                console.printError("Поля продукта не валидны! Продукт не создан!");
+            } finally {
+                console.printError("Непредвиденная ошибка");
+            }
+        }
     }
 }
